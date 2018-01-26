61bf24b4d9e983cc9f137b4ec7cff67e1c583545
Simone de Blasiis <simone.deblasiis@gmail.com>
2018-01-26 11:26:22 +0100

    App

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require('express'),
     bodyParser = require('body-parser');
+const checker = require('./checker.js');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,7 +12,15 @@ app.set('port', (process.env.PORT || 5000));
 // a useless function that returns a fixed object. you can use it, if you want, for testing purposes
 app.get('/count',function (req, res) {
     res.json({count: 5})
-})
+});
+app.post('/check', function (req, res) {
+    var url = req.body.url;
+    var invocPar = req.body.invocationParameters;
+    var expData = req.body.expectedResultData;
+    var expSta = req.body.expectedResultStatus;
+    res.json(checker.check(url,invocPar,expData,expSta));
+
+});
 
 app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
